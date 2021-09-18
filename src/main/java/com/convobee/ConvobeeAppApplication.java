d9928765787965b2394bf9c534dd39f9956ea8d9
vinothkumar.kn <vinothkumar.kn@zohocorp.com>
2021-09-18 09:42:24 +0530

    First commit after initializing project with dependencies

---
+++ src/main/java/com/convobee/ConvobeeAppApplication.java
@@ -0,0 +1,13 @@
+package com.convobee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvobeeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConvobeeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/convobee/ConvobeeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.convobee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvobeeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
